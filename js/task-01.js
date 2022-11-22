4c9b24eb19dae183e1f1b5b4ac7626591ede6b04
annared <annared1982@gmail.com>
2022-11-22 08:37:32 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const numberOfCategories = document.querySelectorAll('.item');
-console.log('Number of categories: ', numberOfCategories.length);
+const categories = document.querySelectorAll('.item');
+console.log('Number of categories: ', categories.length);
+
+categories.forEach(elem => {
+  const title = elem.querySelector('h2');
+  console.log('Category: ', title.textContent);
+
+  const elements = elem.querySelectorAll('li');
+  console.log('Elements:', elements.length);
+});
